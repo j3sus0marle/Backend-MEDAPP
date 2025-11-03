1c691c19744b8533f5def24509d4d2ad4035f809
vosorio18006 <vosorio@uabc.edu.mx>
2025-11-03 00:24:33 -0800

    Conexión MongoDB

--- database.py
+++ database.py
@@ -2,17 +2,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
 
-# Cargar variables de entorno
 load_dotenv()
 
-# Configuración de MongoDB
-MONGO_URL = os.getenv("MONGO_URL")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
+MONGO_URL = os.getenv("MONGO_URI", "mongodb+srv://omarleal_db_user:sBq4WiRLEe1NhCkH@cluster0.zv5i3qo.mongodb.net/APPMEDDB")
+DATABASE_NAME = "APPMEDDB"
 
-# Cliente de MongoDB
 client = AsyncIOMotorClient(MONGO_URL)
 database = client[DATABASE_NAME]
 
 # Colecciones
 campo_info_collection = database.get_collection("Campo_Informativo")
 info_pack_collection = database.get_collection("Paquete_Informacion")
+regiones_collection = database.get_collection("regiones")
